c9a8b5678115b038eec8416515d8509059301b6e
Arturo Barba <arturo.barba@iesdonana.org>
2018-10-03 21:09:21 +0200

    Archivo de prueba

---
+++ tema4/prueba.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba</title>
+  </head>
+  <body>
+    <?php
+     echo "Hola mundo";
+    ?>
+  </body>
+
+</html>
